0f0966dec3d0b5d021a0403e9dfa0f0300d14f49
Olga M. <olga3emes@gmail.com>
2026-01-22 22:39:57 +0100

    Ejemplo 2

--- lib/main.dart
+++ lib/main.dart
@@ -10,23 +10,22 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return  MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Analogía Lego'),
-        ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Icon(Icons.build, size: 50,color: Colors.blue,),
-              SizedBox(height: 20),
-              Text(
-                'Todo es un Widget',
-                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-              ),
-            ],
-          ),
-        ),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home Page'),
+      ),
+      body: Center(
+        child: Text('Welcome to the Home Page!'),
       ),
     );
   }
