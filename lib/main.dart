b2ef62bf987603a4f5826d5557647866439c7908
Olga M. <olga3emes@gmail.com>
2026-01-23 12:09:03 +0100

    4

--- lib/main.dart
+++ lib/main.dart
@@ -4,65 +4,53 @@ void main() {
   runApp(const MainApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+// Definición del widget personalizado
+class BotonPersonalizado extends StatelessWidget {
+  
+  final String texto;
+  final Color color;
+
+  const BotonPersonalizado({super.key, required this.texto, required this.color});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      //home: MiWidget(),
-      home: Contador(),
-    );
-  }
-}
-
-/* ---------- WIDGET SIN ESTADO ---------- */
-class MiWidget extends StatelessWidget {
-  const MiWidget({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const Text("Soy un widget sin estado");
-  }
-}
-
-/* ---------- WIDGET CON ESTADO ---------- */
-class Contador extends StatefulWidget {
-  const Contador({super.key});
-
-  @override
-  State<Contador> createState() => _ContadorState();
-}
-
-class _ContadorState extends State<Contador> {
-  int contador = 0;
-
-  void incrementar() {
-    setState(() {
-      contador++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('Contador')),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              'Contador: $contador',
-              style: const TextStyle(fontSize: 24),
-            ),
-            const SizedBox(height: 16),
-            ElevatedButton(
-              onPressed: incrementar,
-              child: const Text('Incrementar'),
-            ),
-          ],
+    return Container(
+      padding: EdgeInsets.all(12),
+      decoration: BoxDecoration(
+        color: color,
+        borderRadius: BorderRadius.circular(8), // Bordes redondeados
+      ),
+      child: Text(
+        texto,
+        style: const TextStyle(
+          color: Colors.white,
+          fontSize: 16,
+          fontWeight: FontWeight.bold,
         ),
+        textAlign: TextAlign.center,
       ),
     );
   }
+}
+
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+  @override
+  Widget build(BuildContext context) {  
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Ejemplo de Widget Personalizado'),
+        ),
+        body: Center(
+          child: BotonPersonalizado(
+            texto: 'Presióname',
+            color: Colors.blue,
+          ),
+        ),
+      ),
+    );
+  } 
+
 }
