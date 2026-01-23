139fe7bd1bd4214fec4cc68d15a54e850bef55fc
Olga M. <olga3emes@gmail.com>
2026-01-23 11:46:55 +0100

    3

--- lib/main.dart
+++ lib/main.dart
@@ -9,32 +9,60 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  MaterialApp(
-      home: HomePage(),
+    return const MaterialApp(
+      //home: MiWidget(),
+      home: Contador(),
     );
   }
 }
 
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+/* ---------- WIDGET SIN ESTADO ---------- */
+class MiWidget extends StatelessWidget {
+  const MiWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text("Soy un widget sin estado");
+  }
+}
+
+/* ---------- WIDGET CON ESTADO ---------- */
+class Contador extends StatefulWidget {
+  const Contador({super.key});
+
+  @override
+  State<Contador> createState() => _ContadorState();
+}
+
+class _ContadorState extends State<Contador> {
+  int contador = 0;
+
+  void incrementar() {
+    setState(() {
+      contador++;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Home Page'),
-      ),
+      appBar: AppBar(title: const Text('Contador')),
       body: Center(
-        child: Text('Welcome to the Home Page!'),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Contador: $contador',
+              style: const TextStyle(fontSize: 24),
+            ),
+            const SizedBox(height: 16),
+            ElevatedButton(
+              onPressed: incrementar,
+              child: const Text('Incrementar'),
+            ),
+          ],
+        ),
       ),
     );
   }
 }
-
-// Explicación del código
-// ●	main() → función principal que lanza la app con runApp().
-// ●	MyApp → widget raíz que define el diseño global.
-// ●	MaterialApp → proporciona navegación, temas, rutas, etc.
-// ●	Scaffold → estructura visual estándar con AppBar, Body, Drawer, etc.
-// ●	HomePage → pantalla principal.
-
